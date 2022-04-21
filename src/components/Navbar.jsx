b3076f9936493572b30f94e21cb18b6ee7189b15
B0AY-C0D3GR0UND <99433641+B0AY-C0D3GR0UND@users.noreply.github.com>
2022-04-21 08:15:25 +0500

    working on line chart component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
 import {Button ,Menu, Typography , Avatar} from 'antd';
 import {Link } from 'react-router-dom';
 import {HomeOutlined , MoneyCollectOutlined , BulbOutlined , FundOutlined , MenuOutlined} from '@ant-design/icons'
 import icon from '../images/cryptocurrency.png';
 
 const Navbar = () => {
+    const [activeMenu , setactiveMenu] =useState(true);
+    const [screenSize , setscreenSize] = useState(null);
+
+    useEffect(() => {
+     const handleResize = () => setscreenSize(window.innerWidth);
+     window.addEventListener('resize',handleResize)
+     handleResize();
+     return () => window.removeEventListener('resize',handleResize);
+    }, [])
+
+    useEffect(() => {
+      if(screenSize<768){
+          setactiveMenu(false);
+      }
+      else{
+          setactiveMenu(true);
+      }
+    }, [screenSize])
+    
+    
   return (
     <div className="nav-container">
         <div className="logo-container">
@@ -12,10 +32,11 @@ const Navbar = () => {
             <Typography.Title level={2} className = 'logo'>
                 <Link  to="/">Cryptoverse</Link>
             </Typography.Title>
-            {/* <Button className="menu-control-container ">
-
-            </Button> */}
+            <Button className="menu-control-container " onClick={()=> setactiveMenu(true)}>
+                <MenuOutlined/>
+            </Button>
         </div>
+        {activeMenu && (
         <Menu theme='dark'>
             <Menu.Item icon={<HomeOutlined/>}>
                 <Link to="/">Home</Link>
@@ -30,6 +51,7 @@ const Navbar = () => {
                 <Link to="/news">News</Link>
             </Menu.Item>
         </Menu>
+        )}
     </div>
 
 
